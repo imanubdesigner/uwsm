daa9be1ca0385ef74d85fe14dbf5cdc2db041d9b
imanubdesigner <imanubdesigner@gmail.com>
2025-09-11 09:47:12 +0200

    Some fix

--- scripts/installer/final.sh
+++ scripts/installer/final.sh
@@ -7,10 +7,10 @@ BASE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../")
 source $BASE_DIR/scripts/installer/helper.sh
 
 log_message "Final setup script started"
-print_bold_blue "\nCongratulations! Mini Hypr setup is complete! Reboot is recommended"
+print_bold_blue "\nCongratulations! Hyprland-uwsm setup is complete! Reboot is recommended"
 
 print_bold_blue "\nRepository Information:"
-echo "   - GitHub Repository: https://github.com/imanubdesigner/mini-hypr"
+echo "   - GitHub Repository: https://github.com/imanubdesigner/uwsm"
 echo "   - This is an experiment. Seems to work well. Changes will be made to make it even better."
 
 print_success "\nEnjoy your new Hyprland environment! Reboot now for the best experience <3"
--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -11,7 +11,7 @@ print_info "\nStarting utilities setup..."
 
 # -------------------- Utilities --------------------
 run_command "pacman -S --noconfirm cliphist waybar" "Install Waybar and cliphist" "yes"
-run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
+# run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
 run_command "yay -S --sudoloop --noconfirm swww" "Install SWWW for wallpaper management" "yes" "no"
 run_command "yay -S --sudoloop --noconfirm wttrbar" "Install Wttrbar weather for waybar" "yes" "no"
 run_command "yay -S --sudoloop --noconfirm waypaper" "Install Waypaper - Wallpaper tool" "yes" "no"
@@ -31,7 +31,7 @@ chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.local/share/applications" \
   "Create applications folder and copy Neovim desktop files from assets" "yes" "no"
 
 run_command "cp -r $BASE_DIR/configs/waybar /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/waybar" "Copy Waybar config" "yes" "no"
-run_command "cp -r $BASE_DIR/configs/tofi /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/tofi" "Copy Tofi config(s)" "yes" "no"
+# run_command "cp -r $BASE_DIR/configs/tofi /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/tofi" "Copy Tofi config(s)" "yes" "no"
 run_command "cp -r $BASE_DIR/configs/waypaper /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/waypaper" "Copy Waypaper config" "yes" "no"
 
 echo "------------------------------------------------------------------------"
