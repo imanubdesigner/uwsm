f00b3db34cf720f49bff48215fd4bec69494bd23
Manuel B. <imanubdesigner@gmail.com>
2025-09-17 13:44:41 +0200

    Fix command

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -45,11 +45,11 @@ run_command "cp $BASE_DIR/assets/helpers.rc /home/$SUDO_USER/.config/xfce4/helpe
 run_command "chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/xfce4/helpers.rc" "Set correct ownership for helpers.rc" "yes" "no"
 
 # -------------------- Wallpapers --------------------
-run_command "cp $BASE_DIR/assets/wallpapers /home/$SUDO_USER/Pictures/" "Copy wallpapers folder" "yes" "no"
+run_command "cp -r $BASE_DIR/assets/wallpapers /home/$SUDO_USER/Pictures/" "Copy wallpapers folder" "yes" "no"
 
 # -------------------- Swww current Wallpaper --------------------
 run_command "mkdir -p /home/$SUDO_USER/.cache" "Create a swww cache folder for current wallpaper" "yes" "no"
-run_command "cp $BASE_DIR/assets/swww /home/$SUDO_USER/.cache/" "Copy swww cache folder for wallpaper" "yes" "no"
+run_command "cp -r $BASE_DIR/assets/swww /home/$SUDO_USER/.cache/" "Copy swww cache folder for wallpaper" "yes" "no"
 run_command "chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.cache/swww/" "Set correct ownership on swww folder" "yes" "no"
 
 # -------------------- Post-install instructions --------------------
