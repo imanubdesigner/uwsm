37af3f29fd60928ba8db1dcd8aa35e641f53a0d3
Manu <imanubdesigner@gmail.com>
2025-10-27 08:54:59 +0100

    Removed cliphist from hypr.sh script

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -10,6 +10,6 @@ log_message "Installation started for hypr section"
 print_info "\nStarting hypr setup..."
 print_info "\nEverything is recommended to INSTALL"
 
-run_command "pacman -S --noconfirm hyprland hyprland-qtutils wtype wf-recorder satty jq grim slurp wl-clipboard libnotify cliphist waybar hyprpicker hypridle hyprlock uwsm libnewt xdg-desktop-portal-gtk xdg-desktop-portal-hyprland qt5-remoteobjects hyprland-qtutils qt5-wayland qt6-wayland hyprsunset hyprpolkitagent dunst" "Install complete Hyprland ecosystem (core, session manager, wayland support, addons)" "yes"
+run_command "pacman -S --noconfirm hyprland hyprland-qtutils wtype wf-recorder satty jq grim slurp wl-clipboard libnotify waybar hyprpicker hypridle hyprlock uwsm libnewt xdg-desktop-portal-gtk xdg-desktop-portal-hyprland qt5-remoteobjects hyprland-qtutils qt5-wayland qt6-wayland hyprsunset hyprpolkitagent dunst" "Install complete Hyprland ecosystem (core, session manager, wayland support, addons)" "yes"
 
 echo "------------------------------------------------------------------------"
