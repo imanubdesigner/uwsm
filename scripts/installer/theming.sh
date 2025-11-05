a9a865e87ec3193000f8a45e5f1dd6e80f2d0f97
Manuel <146331681+imanubdesigner@users.noreply.github.com>
2025-11-05 22:33:42 +0100

    Update LACT installation command in theming.sh

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -155,7 +155,7 @@ print_info "The installation will automatically compile DKMS modules and rebuild
 
 run_command "pacman -S --noconfirm --needed linux-headers nvidia-open-dkms nvidia-utils libva libva-utils lib32-nvidia-utils libva-nvidia-driver egl-wayland mesa lib32-mesa" "Install Nvidia Open DKMS (will auto-run mkinitcpio)" "yes"
 
-run_command "pacman -S --noconfirm lact && systemctl enable --now lactd" "Install LACT for GPU" "yes"
+run_command "pacman -S --noconfirm --needed lact && systemctl enable --now lactd" "Install LACT for GPU" "yes"
 
 print_success "NVIDIA drivers installed and initramfs rebuilt automatically!"
 
