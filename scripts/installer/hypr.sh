436b8c84244897594c196d7aef98145120f41ec2
Manu <imanubdesigner@gmail.com>
2025-11-16 13:35:13 +0100

    Hyprland-guituls stuff

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -10,7 +10,7 @@ log_message "Installation started for hypr section"
 print_info "\nStarting hypr setup..."
 print_info "\nEverything is recommended to INSTALL"
 
-run_command "pacman -S --needed --noconfirm hyprland hyprland-guiutils satty jq grim slurp wl-clipboard libnotify waybar hyprpicker hypridle hyprlock uwsm libnewt xdg-desktop-portal-gtk xdg-desktop-portal-hyprland qt5-remoteobjects hyprland-qtutils qt5-wayland qt6-wayland hyprsunset hyprpolkitagent mako" "Install complete Hyprland ecosystem (core, session manager, wayland support, addons)" "yes"
+run_command "pacman -S --needed --noconfirm hyprland hyprland-guiutils satty jq grim slurp wl-clipboard libnotify waybar hyprpicker hypridle hyprlock uwsm libnewt xdg-desktop-portal-gtk xdg-desktop-portal-hyprland qt5-remoteobjects qt5-wayland qt6-wayland hyprsunset hyprpolkitagent mako" "Install complete Hyprland ecosystem (core, session manager, wayland support, addons)" "yes"
 
 run_command "systemctl --user enable waybar.service" \
   "Enable Waybar packaged user unit (no start now)" "yes" "no"
