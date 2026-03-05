418583f85d549395823673c00a179e64a157332c
Manuel B. <imanubdesigner@gmail.com>
2026-03-05 21:01:14 +0100

    Typo

--- configs/nvim/lua/plugins/coloscheme.lua
+++ configs/nvim/lua/plugins/coloscheme.lua
@@ -23,7 +23,7 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "mocha", -- Selected flavour: latte, frappe, macchiato, mocha
-        transparent_background = false,
+        transparent_background = true,
         integrations = {
           lualine = true,
           treesitter = true,
