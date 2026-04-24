195b486e71c046ccc1a757e8feaa95bb60fab286
Manuel B. <imanubdesigner@gmail.com>
2026-04-24 08:57:35 +0200

    Added repo

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -10,7 +10,28 @@ log_message "Installation started for utilities section"
 print_info "\nStarting utilities setup..."
 
 # -------------------- Utilities --------------------
-run_command "yay -S --sudoloop --noconfirm --needed helium-browser-bin hyprland-preview-share-picker-git python-terminaltexteffects xdg-terminal-exec localsend-bin walker elephant elephant-desktopapplications elephant-files elephant-websearch elephant-clipboard elephant-calc elephant-runner elephant-symbols elephant-unicode elephant-providerlist elephant-menus elephant-todo fastmod yaru-icon-theme gowall limine-mkinitcpio-hook limine-snapper-sync" "Install Utilities" "no" "no"
+# run_command "yay -S --sudoloop --noconfirm --needed helium-browser-bin hyprland-preview-share-picker-git python-terminaltexteffects xdg-terminal-exec localsend-bin walker elephant elephant-desktopapplications elephant-files elephant-websearch elephant-clipboard elephant-calc elephant-runner elephant-symbols elephant-unicode elephant-providerlist elephant-menus elephant-todo fastmod yaru-icon-theme gowall limine-mkinitcpio-hook limine-snapper-sync" "Install Utilities" "no" "no"
+
+# Pacman
+run_command "pacman -S --needed --noconfirm \
+  walker \
+  elephant elephant-desktopapplications elephant-files elephant-websearch \
+  elephant-clipboard elephant-calc elephant-runner elephant-symbols \
+  elephant-unicode elephant-providerlist elephant-menus elephant-todo \
+  hyprland-preview-share-picker-git \
+  python-terminaltexteffects \
+  localsend-bin \
+  yaru-icon-theme \
+  xdg-terminal-exec \
+  gpu-screen-recorder" \
+  "Install packages from omarchy repo" "no"
+
+# AUR
+run_command "yay -S --sudoloop --noconfirm --needed \
+  helium-browser-bin \
+  gowall \
+  fastmod" \
+  "Install remaining AUR packages" "no" "no"
 
 # -------------------- Applications & mimeinfo --------------------
 run_command "\
