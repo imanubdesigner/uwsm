82f9a79b7b0d45eddce460f7a2d77978512b22ea
Manuel B. <imanubdesigner@gmail.com>
2026-04-26 14:39:18 +0200

    Added hooks files

---
+++ configs/manu/hooks/theme-set.d/10-vicinae.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+output_file="$HOME/.config/manu/current/theme/vicinae.toml"
+
+if ! command -v vicinae >/dev/null 2>&1; then
+    skipped "Vicinae"
+fi
+
+if [ ! -f "$output_file" ]; then
+cat > "$output_file" << EOF
+[meta]
+version = 1
+name = "Manu"
+description = "Follow your Manu system theme."
+variant = "dark"
+inherits = "vicinae-dark"
+icon = "$HOME/.local/share/manu/icon.png"
+
+[colors.core]
+background = "#${primary_background}"
+foreground = "#${primary_foreground}"
+secondary_background = "#$(change_shade $primary_background 5)"
+border = "#${bright_black}"
+accent = "#${normal_yellow}"
+
+[colors.list.item.hover]
+background = "#$(change_shade $primary_background 10)"
+foreground = "#${primary_foreground}"
+
+[colors.list.item.selection]
+background = "#$(change_shade $primary_background 15)"
+foreground = "#${primary_foreground}"
+
+[colors.accents]
+blue = "#${normal_blue}"
+green = "#${normal_green}"
+magenta = "#${normal_magenta}"
+orange = "#${normal_yellow}"
+purple = "#${bright_magenta}"
+red = "#${normal_red}"
+yellow = "#${bright_yellow}"
+cyan = "#${normal_cyan}"
+EOF
+fi
+
+mkdir -p "$HOME/.local/share/vicinae/themes/"
+cp -p -f "$output_file" "$HOME/.local/share/vicinae/themes/manu.toml"
+
+vicinae theme set manu
+
+success "Vicinae theme updated!"
+exit 0
